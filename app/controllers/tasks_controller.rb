e307643cb2b7df9f6f7142fdd6fc819c64b05b72
gade.nahi <gadenahi@gmail.com>
2018-05-21 08:36:48 +0000

    add column status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,53 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @tasks = Task.all
   end
   
   def show
   end
   
   def new
+    @task = Task.new
   end
   
   def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'Taskが正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Taskが投稿されませんでした'
+      render :new
+    end
   end
-  
+
   def edit
   end
   
   def update
+    if @task.update(task_params)
+      flash[:success] = 'Taskが正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Taskが投稿されませんでした'
+      render :new
+    end
   end
   
   def destroy
+    @task.destroy
   end
+
+private
+def set_task
+  @task = Task.find(params[:id])
+end
+
+def task_params
+  params.require(:task).permit(:status)
+end
 
 end
